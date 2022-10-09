1982da3ce14e5618eb076cb1a74b010a4d9e07bb
Naisato <Natdadai.t@ku.th>
2022-10-09 21:25:25 +0700

    create report resource

--- app/Http/Resources/ReportResource.php
+++ app/Http/Resources/ReportResource.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Resources;
 
+use App\Models\User;
 use Illuminate\Http\Resources\Json\JsonResource;
 
 class ReportResource extends JsonResource
@@ -9,11 +10,31 @@ class ReportResource extends JsonResource
     /**
      * Transform the resource into an array.
      *
-     * @param  \Illuminate\Http\Request  $request
+     * @param \Illuminate\Http\Request $request
      * @return array|\Illuminate\Contracts\Support\Arrayable|\JsonSerializable
      */
     public function toArray($request)
     {
-        return parent::toArray($request);
+        return [
+            'id' => $this->id,
+            'user_report_id' => UserResource::collection($this->whenLoaded('users')),
+            'reportable_id' => UserResource::collection($this->whenLoaded('users')),
+            'reportable_type' => $this->when($this->whenLoaded('reportable'), function () {
+                if ($this->reportable_type->reportable instanceof App\Artwork) {
+                    return new ArtworkResource($this->reportable_type->reportable);
+                }
+                if ($this->reportable_type->reportable instanceof App\User) {
+                    return new UserResource($this->reportable_type->reportable);
+                }
+
+                if ($this->reportable_type->reportable instanceof App\Comment) {
+                    return new CommentResource($this->reportable_type->reportable);
+                }
+
+                return null;
+            }),
+
+            'description' => $this->description,
+        ];
     }
 }
