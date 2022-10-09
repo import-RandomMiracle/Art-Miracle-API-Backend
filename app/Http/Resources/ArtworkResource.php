8ebeccfb767da648a754ce5908e58df1b376a7f3
Naisato <Natdadai.t@ku.th>
2022-10-09 20:48:13 +0700

    create resource artist&artwork

---
+++ app/Http/Resources/ArtworkResource.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Http\Resources;
+
+use Illuminate\Http\Resources\Json\JsonResource;
+
+class ArtworkResource extends JsonResource
+{
+    /**
+     * Transform the resource into an array.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return array|\Illuminate\Contracts\Support\Arrayable|\JsonSerializable
+     */
+    public function toArray($request)
+    {
+        return [
+            'id' => $this->id,
+            'art_name' => $this->art_name,
+            'path' => $this->path,
+            'description' => $this->description,
+        ];
+    }
+}
