cdf3d3b76e22850cc9e7c323bbb8d82fc177498b
Naisato <Natdadai.t@ku.th>
2022-10-09 21:40:18 +0700

    create controller artist

---
+++ app/Http/Controllers/Api/ArtistController.php
@@ -0,0 +1,88 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Http\Controllers\Controller;
+use App\Http\Resources\ArtistResource;
+use App\Models\Artist;
+use Illuminate\Http\Request;
+
+class ArtistController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        $artists = Artist::get();
+        return ArtistResource::collection($artists);
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\Artist  $artist
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Artist $artist)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Models\Artist  $artist
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(Artist $artist)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Models\Artist  $artist
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, Artist $artist)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\Artist  $artist
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Artist $artist)
+    {
+        //
+    }
+}
