ec899ebf380d78e82f91d51370758a70282a6333
Naisato <Natdadai.t@ku.th>
2022-10-29 15:10:35 +0700

    create like storage

---
+++ app/Http/Controllers/Api/ImageController.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Http\Controllers\Controller;
+
+class ImageController extends Controller
+{
+    public function index()
+    {
+
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use Illuminate\Support\Facades\Storage;
 
 /*
 |--------------------------------------------------------------------------
@@ -15,4 +16,10 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
+});
+
+Route::get('/test', function () {
+    $url = Storage::url('nanashi-mumei.gif');
+    $image = Storage::get('nanashi-mumei.gif');
+    return view('test',['url' => $url,'image' => $image]);
 });
