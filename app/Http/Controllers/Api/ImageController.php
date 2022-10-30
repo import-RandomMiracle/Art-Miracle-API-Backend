c9fb351b33cce7ac6e24eded8bfa56193aa2c165
Naisato <Natdadai.t@ku.th>
2022-10-30 21:35:32 +0700

    save image and resize image

--- app/Http/Controllers/Api/ImageController.php
+++ app/Http/Controllers/Api/ImageController.php
@@ -3,9 +3,11 @@
 namespace App\Http\Controllers\Api;
 
 use App\Http\Controllers\Controller;
+use App\Models\Artwork;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Response;
 use Illuminate\Support\Facades\Storage;
+use Intervention\Image\Facades\Image;
 
 class ImageController extends Controller
 {
@@ -20,24 +22,50 @@ class ImageController extends Controller
 //            'file' => 'required|mimes:png,jpg,jpeg,csv,txt,xlx,xls,pdf|max:2048'
 //        ]);
 
-        if($request->hasFile('avatar')){
-            $image      = $request->file('avatar');
-            $fileName   = time() . '.' . $image->getClientOriginalExtension();
+        if ($request->hasFile('avatar')) {
+            $image = $request->file('avatar');
+            $ext = $image->getClientOriginalExtension();
+            $fileName = time() . '.' . $ext;
+
             $path = $image->storeAs('avatar',$fileName,'public');
+
+
+            $image = Image::make($image);
+
+            $image->resize(200, 200, function ($constraint) {
+                $constraint->aspectRatio();
+            })->encode($ext);
+
+//            $hash = md5($image->__toString()) . $ext;
+
+            Storage::put('public/avatar_resize/' . 'resize_' . $fileName,$image->__toString());
         }
 
         return $fileName;
     }
 
-    public function download(){
+    public function download()
+    {
         return Storage::download('public/nanashi-mumei.gif');
     }
 
-    public function delete(){
+    public function delete()
+    {
         return Storage::delete('public/image/OsDOp5damDFQK2D0B5NZalTAd6sGTe60Rdr6M3R8.jpg');
     }
 
-    public function edit(Request $request){
-        $path = $request->file('avatar')->storeAs('image','test.jpg', 'public');
+    public function edit(Request $request)
+    {
+        $path = $request->file('avatar')->storeAs('image', 'test.jpg', 'public');
+    }
+
+    public function getRealImage(Request $request) {
+        $artwork = Artwork::find($request->id);
+        return $artwork->path;
+    }
+
+    public function getShowImage(Request $request) {
+        $artwork = Artwork::find($request->id);
+        return $artwork->path;
     }
 }
